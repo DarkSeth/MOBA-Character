bd043f8c8380ba91ea0e1e081e385865cfc9771f
vinceyangeles <30303455+vinceyangeles@users.noreply.github.com>
2020-07-13 17:38:20 +1000

    MOBA Character YT Series (E05)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,20 +6,36 @@ using UnityEngine.AI;
 public class Movement : MonoBehaviour
 {
 
-    NavMeshAgent agent;
+    public NavMeshAgent agent;
 
     public float rotateSpeedMovement = 0.1f;
-    float rotateVelocity;
+    public float rotateVelocity;
+
+    private HeroCombat heroCombatScript;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        heroCombatScript = GetComponent<HeroCombat>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(heroCombatScript.targetedEnemy != null)
+        {
+            if(heroCombatScript.targetedEnemy.GetComponent<HeroCombat>() != null)
+            {
+                if (!heroCombatScript.targetedEnemy.GetComponent<HeroCombat>().isHeroAlive)
+                {
+                    heroCombatScript.targetedEnemy = null;
+                }
+            }
+
+        }
+
+
         //When pressing the left mouse button
         if(Input.GetMouseButtonDown(1))
         {
@@ -28,18 +44,23 @@ public class Movement : MonoBehaviour
             //Checking if the raycast shot hits something that uses the navmesh system.
             if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
             {
-                //MOVEMENT
-                //Have the player move to the raycast/hit point.
-                agent.SetDestination(hit.point);
+                if(hit.collider.tag == "Floor")
+                {
+                    //MOVEMENT
+                    agent.SetDestination(hit.point);
+                    heroCombatScript.targetedEnemy = null;
+                    agent.stoppingDistance = 0;
 
-                //ROTATION
-                Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
-                float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,
-                    rotationToLookAt.eulerAngles.y,
-                    ref rotateVelocity,
-                    rotateSpeedMovement * (Time.deltaTime * 5));
+                    //ROTATION
+                    Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                    float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y,
+                        rotationToLookAt.eulerAngles.y,
+                        ref rotateVelocity,
+                        rotateSpeedMovement * (Time.deltaTime * 5));
 
-                transform.eulerAngles = new Vector3(0, rotationY, 0);
+                    transform.eulerAngles = new Vector3(0, rotationY, 0);
+                }
+
             }
 
 

7ec7c9771c998843584ff146032ce674c90f4089
vinceyangeles <30303455+vinceyangeles@users.noreply.github.com>
2021-03-03 01:24:03 +1100

    MOBA Ability (Wukong W - E03)

---
+++ Assets/Ability Series/WarriorTrickster.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarriorTrickster : MonoBehaviour
+{
+    //In Script Stuff
+    Animator anim;
+    Movement moveScript;
+
+    [Header("Warrior Trickster Ability")]
+    public Image cloneImage;
+    public float cooldown = 10;
+    bool isCooldown = false;
+    bool canSpawnClone = true;
+    public KeyCode ability;
+
+    [Space(5)]
+    public bool inInvisMode = false;
+    public Transform cloneSpawnPoint;
+    public GameObject cloneObj;
+    public GameObject invisCanvasObj;
+
+    [Header("Body for Material Change")]
+    public Material transparentMaterial;
+    public SkinnedMeshRenderer skinMeshBod1;
+    public SkinnedMeshRenderer skinMeshBod2;
+    public Material bod1Material;
+    public Material bod2Material;
+    
+
+    void Start()
+    {
+        cloneImage.fillAmount = 0;
+
+        moveScript = GetComponent<Movement>();
+        anim = GetComponent<Animator>();
+
+        skinMeshBod1.material = bod1Material;
+        skinMeshBod2.material = bod2Material;
+
+        invisCanvasObj.SetActive(false);
+    }
+
+    void Update()
+    {
+        CloneAbility();
+    }
+
+    public void CloneAbility()
+    {
+        //Press the ability
+        if (Input.GetKey(ability))
+        {
+            if (canSpawnClone && isCooldown == false)
+            {
+                isCooldown = true;
+                cloneImage.fillAmount = 1;
+                inInvisMode = true;
+
+                StartCoroutine(SpawnClone());
+            }
+
+        }
+
+        //Turn invislbe
+        if(inInvisMode)
+        {
+            invisCanvasObj.SetActive(true);
+            skinMeshBod1.material = transparentMaterial;
+            skinMeshBod2.material = transparentMaterial;
+        }
+        else
+        {
+            skinMeshBod1.material = bod1Material;
+            skinMeshBod2.material = bod2Material;
+            invisCanvasObj.SetActive(false);
+        }
+
+        //Icon ability goes on cooldown
+        if (isCooldown)
+        {
+            Debug.Log("YEEEP");
+            cloneImage.fillAmount -= 1 / cooldown * Time.deltaTime;
+
+            if (cloneImage.fillAmount <= 0)
+            {
+                cloneImage.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
+    }
+
+    IEnumerator SpawnClone()
+    {
+        canSpawnClone = false;
+        Instantiate(cloneObj, cloneSpawnPoint.transform.position, cloneSpawnPoint.transform.rotation);
+        yield return new WaitForSeconds(cooldown);
+        canSpawnClone = true;
+    }
+}
---
+++ Assets/WarriorTricksterClone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorTricksterClone : MonoBehaviour
+{
+    WarriorTrickster wtScript;
+
+    private void Start()
+    {
+        wtScript = GameObject.FindGameObjectWithTag("Player").GetComponent<WarriorTrickster>();
+    }
+
+    void Update()
+    {
+        StartCoroutine(Destroy());
+    }
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSeconds(3);
+        wtScript.inInvisMode = false;
+        Destroy(gameObject);
+    }
+}
